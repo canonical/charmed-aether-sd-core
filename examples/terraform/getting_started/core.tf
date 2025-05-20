ea55aa2a83cd05fc6c61cc0148c2ad04875f8b56
gatici <gulsum.atici@canonical.com>
2025-05-20 16:22:08 +0300

    chore: remove router from sdcore deployment
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- examples/terraform/getting_started/core.tf
+++ examples/terraform/getting_started/core.tf
@@ -5,18 +5,11 @@ resource "juju_model" "sdcore" {
   name = "sdcore"
 }
 
-module "sdcore-router" {
-  source = "git::https://github.com/canonical/sdcore-router-k8s-operator//terraform"
-
-  model      = juju_model.sdcore.name
-  depends_on = [juju_model.sdcore]
-}
 
 module "sdcore" {
   source = "git::https://github.com/canonical/terraform-juju-sdcore//modules/sdcore-k8s"
 
   model      = juju_model.sdcore.name
-  depends_on = [module.sdcore-router]
 
   traefik_config = {
     routing_mode = "subdomain"
